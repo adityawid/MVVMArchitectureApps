4fd8be3bd8e402be232340588078113fc145a44a
Aditya Widayanto <devjurnal.id@gmail.com>
2019-04-21 23:24:01 +0700

    Initial commit

---
+++ app/src/main/java/com/adityawidayanto/mvvmapps/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.adityawidayanto.mvvmapps.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.adityawidayanto.mvvmapps.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
